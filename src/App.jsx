c94ddee74f92f3002f5e48af1102c1a040338c73
Ayushman <avenuedesign768@gmail.com>
2026-09-17 20:07:14 +0530

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Navigation from "./layout/Navigation";
 import Footer from "./layout/Footer";
 import Home from "./screens/Home";
+import { BrowserRouter, Routes, Route } from "react-router";
+import About from "./screens/About";
+import Product from "./screens/Product";
+import Contact from "./screens/Contact";
 
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <Home/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+        <Navigation />
+        {/* Route Area */}
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/product" element={<Product />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   )
 }
 
--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,12 +1,32 @@
+import { Links } from "react-router";
+
+
 function Navigation() {
     return (
         <>
-            <nav className="navbar bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container">
-                    <a className="navbar-brand" href="#">
-                        <img src="https://thecodemunk.in/assets/logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
-                            TCM One
-                    </a>
+                    <Link to="/" className="navbar-brand">Website Logo</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/about" className="nav-link active" aria-current="page">Home</Link>
+                            </li>
+                              <li className="nav-item">
+                                <Link to="/about" className="nav-link " aria-current="page">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/product" className="nav-link " aria-current="page">Product</Link>
+                            </li>
+                            <li className="nav-item">
+                               <Link to="/contact" className="nav-link " aria-current="page">Contact</Link>
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </>
